bf07754d10d277a89b73dec53c7002f4a3d3247d
Joe Mancera <joeharrymancera@gmail.com>
2022-02-09 13:35:25 -0500

    Add contentfulProjects API and implement that on projects page

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -2,11 +2,28 @@ import { Head } from "components/Head";
 import { CommonLayout } from "components/CommonLayout";
 import { Hero } from "components/Hero";
 import { ContentfulHeros } from "@contentful/contentfulHeros";
+import { ContentfulProjects } from "@contentful/contentfulProjects";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
-export default function Projects({ hero }) {
+export default function Projects({ hero, projects }) {
+  console.log(projects);
+  const richTextOptions = {
+    renderMark: {
+      [MARKS.BOLD]: (text) => <b>{text}</b>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <p className="relative">{children}</p>
+      ),
+    },
+    renderText: (text) => text.replace("!", "?"),
+  };
+
   return (
     <CommonLayout>
-      <Head description="Curran example">Curran | Home</Head>
+      <Head description="Curran example">Curran | Projects</Head>
 
       <Hero hero={hero} />
       <div>
@@ -16,6 +33,27 @@ export default function Projects({ hero }) {
           animi! Repellat est expedita provident facilis. Deserunt impedit
           inventore recusandae asperiores id?
         </p>
+        <section>
+          <h2>Projects</h2>
+          {projects.map((project) => {
+            return (
+              <div key={project.sys.id} style={{ position: "relative" }}>
+                <Image
+                  src={project.image.url}
+                  alt={project.image.description}
+                  layout="fill"
+                  objectFit="cover"
+                  priority={true}
+                />
+                <h3 className="z-10 relative">{project.title}</h3>
+                {documentToReactComponents(
+                  project.description.json,
+                  richTextOptions
+                )}
+              </div>
+            );
+          })}
+        </section>
       </div>
     </CommonLayout>
   );
@@ -23,7 +61,8 @@ export default function Projects({ hero }) {
 
 export async function getStaticProps() {
   const hero = (await ContentfulHeros.getHeroItems(1)) || {};
+  const projects = (await ContentfulProjects.getProjects()) || [];
   return {
-    props: { hero },
+    props: { hero, projects },
   };
 }
